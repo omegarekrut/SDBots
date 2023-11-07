165422da7309b28cf9f6042b17a20201541bbf48
Vitaliy Kim <vitaliy.kim@resolventagroup.com>
2023-11-07 16:41:45 +0100

    Removed StartCommand from AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Telegram::addCommands([
-            \App\Telegram\Commands\StartCommand::class,
-        ]);
+
     }
 }
