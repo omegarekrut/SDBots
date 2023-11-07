96d62c37d3a2a6764e2cf378753377cf7394327b
Vitaliy Kim <vitaliy.kim@resolventagroup.com>
2023-11-07 17:02:00 +0100

    Added some buttons for StartCommand.php

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -2,7 +2,10 @@
 
 namespace App\Telegram\Commands;
 
+use Telegram\Bot\Actions;
 use Telegram\Bot\Commands\Command;
+use Telegram\Bot\Keyboard\Keyboard;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class StartCommand extends Command
 {
@@ -12,8 +15,44 @@ class StartCommand extends Command
 
     public function handle(): void
     {
+        $this->replyWithChatAction(['action' => Actions::TYPING]);
+
+        $keyboard = Keyboard::make()
+            ->inline()
+            ->row(
+                Keyboard::inlineButton(['text' => 'Button 1', 'callback_data' => 'button1']),
+                Keyboard::inlineButton(['text' => 'Button 2', 'callback_data' => 'button2'])
+            );
+
         $this->replyWithMessage([
-            'text' => 'Hey, there! Welcome to our bot!',
+            'text' => 'Hey there! Welcome to our bot! Choose an option:',
+            'reply_markup' => $keyboard
+        ]);
+    }
+
+    public function handleCallbackQuery($callbackQuery): void
+    {
+        $data = $callbackQuery->getData();
+
+        switch ($data) {
+            case 'button1':
+                $responseText = 'You pressed button 1!';
+                break;
+            case 'button2':
+                $responseText = 'You pressed button 2!';
+                break;
+            default:
+                $responseText = 'Unknown operation';
+                break;
+        }
+
+        Telegram::sendMessage([
+            'chat_id' => $callbackQuery->getMessage()->getChat()->getId(),
+            'text' => $responseText
+        ]);
+
+        Telegram::answerCallbackQuery([
+            'callback_query_id' => $callbackQuery->getId()
         ]);
     }
 }
