b8297e9866bd05d31989e313b019622b3bf9f8f7
Vitaliy Kim <vitaliy.kim@resolventagroup.com>
2023-11-10 08:49:36 +0100

    Added helper for token retrieval from API

---
+++ app/Services/SuperDispatchService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class SuperDispatchService
+{
+    public function getAccessToken(): ?string
+    {
+        $response = Http::asForm()->post('https://carrier.superdispatch.com/oauth/token/', [
+            'grant_type' => 'client_credentials',
+            'client_id' => env('SUPERDISPATCH_CLIENT_ID'),
+            'client_secret' => env('SUPERDISPATCH_CLIENT_SECRET')
+        ]);
+
+        return $response->successful() ? $response->json()['access_token'] : null;
+    }
+
+    public function fetchOrder(string $orderID, string $accessToken): ?array
+    {
+        $response = Http::withHeaders([
+            'Authorization' => "Bearer {$accessToken}",
+            'Content-Type' => 'application/json'
+        ])->get("https://carrier.superdispatch.com/v1/orders/{$orderID}/");
+
+        return $response->successful() ? $response->json() : null;
+    }
+}
