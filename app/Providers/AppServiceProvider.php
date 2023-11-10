3203f4f48339a5519c4a0d632cd0abaf77b36aeb
Vitaliy Kim <vitaliy.kim@resolventagroup.com>
2023-11-10 08:50:13 +0100

    Registered the SuperDispatchService

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\SuperDispatchService;
 use Illuminate\Support\ServiceProvider;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
@@ -12,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(SuperDispatchService::class, function ($app) {
+            return new SuperDispatchService();
+        });
     }
 
     /**
