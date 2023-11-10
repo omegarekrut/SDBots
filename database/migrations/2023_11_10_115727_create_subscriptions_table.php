519adb65f3ec4db61cca854afa5126255b0cb9af
Vitaliy Kim <vitaliy.kim@resolventagroup.com>
2023-11-10 13:14:07 +0100

    Added new table for contain telegram users to send them every hour an error

---
+++ database/migrations/2023_11_10_115727_create_subscriptions_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('telegram_user_id')->unique();
+            $table->boolean('is_subscribed')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
