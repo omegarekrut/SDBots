eaaa37e9dda6cf5d6e3c9a0e61ea2dc0f9948d8e
Vitaliy Kim <vitaliy.kim@resolventagroup.com>
2023-11-16 22:57:16 +0100

    Rewrite /start command to log chat_id

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Telegram\Commands;
 
+use Illuminate\Support\Facades\Log;
 use Telegram\Bot\Commands\Command;
 
 class StartCommand extends Command
@@ -12,6 +13,10 @@ class StartCommand extends Command
 
     public function handle(): void
     {
+        $update = $this->getUpdate();
+        $chat_id = $update->getMessage()->getChat()->getId();
+        Log::info("Received /start from chat ID: " . $chat_id);
+
         $this->replyWithMessage([
             'text' => 'Hey, there! Welcome to our bot!',
         ]);
