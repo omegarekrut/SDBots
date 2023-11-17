dd90ee2b6d2a0e6163b8f4a18446d4d17b1b0f19
Vitaliy Kim <vitaliy.kim@resolventagroup.com>
2023-11-17 01:54:57 +0100

    Fix message formatting in TelegramValidationMessageService
    Adjusted formatValidationResults to correctly handle the structure of the $results array, ensuring error messages are properly appended to the formatted message. This resolves the issue where validation errors were not being displayed in the output message.

--- app/Services/TelegramValidationMessageService.php
+++ app/Services/TelegramValidationMessageService.php
@@ -8,22 +8,22 @@ class TelegramValidationMessageService
 {
     public function formatValidationResults($results, string $orderID, string $carrierName): string
     {
-        if (empty($results)) {
+        $errorObject = $results['App\\Models\\Error'] ?? null;
+
+        if (!$errorObject) {
             return "✅ No errors found for Order ID: {$orderID}";
         }
-        Log::info('Validation results received', ['results' => $results]);
 
-        $formattedMessage = "🔍 Validation results for Order ID: {$results->order_id}\n\n⚡️⚡️⚡️\n\nCompany name: {$carrierName}";
+        $formattedMessage = "🔍 Validation results for Order ID: {$errorObject->order_id}\n\n⚡️⚡️⚡️\n\nCompany name: {$carrierName}";
         $errorMessages = ErrorMessageService::getErrorMessages();
-        Log::info('Formatted message', ['message' => $formattedMessage]);
 
-        foreach ($results as $key => $value) {
+        foreach ($errorObject as $key => $value) {
             if ($this->isValidationErrorKey($key, $value)) {
                 $formattedMessage .= "{$errorMessages[$key]}: ❌ Failed\n";
             }
         }
 
-        return $this->appendErrorMessageOrFinalize($formattedMessage, $results);
+        return $this->appendErrorMessageOrFinalize($formattedMessage, $errorObject);
     }
 
     private function isValidationErrorKey(string $key, $value): bool
